adca7305f4118af5673963bf15c9c2af9efd54de
Emanuel Umbelino <umbelino.emanuel@gmail.com>
2023-03-16 20:03:20 -0300

    Direction of each pixel

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def normalize(vector):
+    return vector / np.linalg.norm(vector)
+
 # dinamic size
 width = 300
 height = 200
 
-# camera position
-camera = np.array([0, 0, 1])
-
+camera = np.array([0, 0, 1]) # camera position
 ratio = float(width) / height
+screen = (-1, 1 / ratio, 1, -1 / ratio) # screen: left, top, right, bottom
 
-# screen: left, top, right, bottom
-screen = (-1, 1 / ratio, 1, -1 / ratio)
 
-# set black image in current size
-image = np.zeros((height, width, 3))
-
-# loop splitting the screen into width and height in the x and y directions
+image = np.zeros((height, width, 3)) # set black image in current size
+# split the screen into width and height in the x and y directions
 for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
     for j, x in enumerate(np.linspace(screen[0], screen[2], width)):
+        # calculate direction of current pixel
+        pixel = np.array([x, y, 0])
+        origin = camera
+        direction = normalize(pixel - origin)
+        
         # compute de color of current pixel
         # image[i, j] = ...
-        print("progress: %d/%d" % (i + 1, height))
+    print("progress: %d/%d" % (i + 1, height))
 
 # convert image
 plt.imsave('image.png', image)
